43a49c227cf1cedd6a3bcb76f2c7cb9d822d5a5a
Théotime <tronaidev@gmail.com>
2023-10-05 12:36:34 -0500

    Updating course api

--- api/controllers/course.controller.js
+++ api/controllers/course.controller.js
@@ -8,18 +8,16 @@ module.exports.getCourse = async (req, res) => {
 
     try {
 
-        //let sql = "SELECT title, SUM(CASE WHEN grade = 'A+' THEN grade_number ELSE 0 END) AS 'A+', SUM(CASE WHEN grade = 'A' THEN grade_number ELSE 0 END) AS 'A', SUM(CASE WHEN grade = 'A-' THEN grade_number ELSE 0 END) AS 'A-', SUM(CASE WHEN grade = 'B+' THEN grade_number ELSE 0 END) AS 'B+', SUM(CASE WHEN grade = 'B' THEN grade_number ELSE 0 END) AS 'B', SUM(CASE WHEN grade = 'B-' THEN grade_number ELSE 0 END) AS 'B-', SUM(CASE WHEN grade = 'C+' THEN grade_number ELSE 0 END) AS 'C+', SUM(CASE WHEN grade = 'C' THEN grade_number ELSE 0 END) AS 'C', SUM(CASE WHEN grade = 'C-' THEN grade_number ELSE 0 END) AS 'C-', SUM(CASE WHEN grade = 'D+' THEN grade_number ELSE 0 END) AS 'D+', SUM(CASE WHEN grade = 'D' THEN grade_number ELSE 0 END) AS 'D', SUM(CASE WHEN grade = 'E' THEN grade_number ELSE 0 END) AS 'E', SUM(CASE WHEN grade = 'F' THEN grade_number ELSE 0 END) AS 'F', SUM(CASE WHEN grade = 'W' THEN grade_number ELSE 0 END) AS 'W', SUM(CASE WHEN grade = '(S)' THEN grade_number ELSE 0 END) AS '(S)', SUM(CASE WHEN grade = '(E)' THEN grade_number ELSE 0 END) AS '(E)' FROM courses WHERE acronym = ? GROUP BY title;"
-
         const notes = ['A+', 'A', 'A-', 'B+', 'B', 'B-', 'C+', 'C', 'C-', 'D+', 'D', 'E', 'F', 'W', '(S)', '(E)'];
         const selectClauses = notes.map((note) => `SUM(CASE WHEN grade = '${note}' THEN grade_number ELSE 0 END) AS '${note}'`);
         const selectClause = selectClauses.join(', ');
 
         const [overall, byProfessor] = await Promise.all([
-            queryDatabase(`SELECT title, acronym, ${selectClause}, GROUP_CONCAT(DISTINCT session ORDER BY session ASC SEPARATOR ', ') AS sessions FROM courses WHERE acronym = ? GROUP BY title;`, [`${acronym}`]),
+            queryDatabase(`SELECT title, acronym, ${selectClause}, GROUP_CONCAT(DISTINCT session ORDER BY session ASC SEPARATOR ', ') AS sessions FROM courses WHERE acronym = ? GROUP BY title LIMIT 1;`, [`${acronym}`]),
             queryDatabase(`SELECT professor, ${selectClause}, GROUP_CONCAT(DISTINCT session ORDER BY session ASC SEPARATOR ', ') AS sessions FROM courses WHERE acronym = ? GROUP BY title, professor;`, [`${acronym}`])
         ]);
 
-        res.status(200).json({ overall, byProfessor });
+        res.status(200).json({ overall: overall[0], byProfessor });
 
     } catch (err) {
         res.status(500).json({ message: "Internal Server Error." + err });
