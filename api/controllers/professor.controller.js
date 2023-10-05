d612da942446b50d5443287126ac90de92472747
Théotime <tronaidev@gmail.com>
2023-10-05 13:06:25 -0500

    Updating professor api

--- api/controllers/professor.controller.js
+++ api/controllers/professor.controller.js
@@ -1,12 +1,24 @@
+const { queryDatabase } = require("../config/db");
+
 module.exports.getProfessor = async (req, res) => {
 
-    try {
-        const professorName = req.params.professor;
+    const professor = req.params.professor;
 
-        res.status(200).json({});
-    } catch (error) {
-        console.error("Erreur :", error);
-        res.status(500).json({ message: "Internal error." });
+    try {
+
+        const notes = ['A+', 'A', 'A-', 'B+', 'B', 'B-', 'C+', 'C', 'C-', 'D+', 'D', 'E', 'F', 'W', '(S)', '(E)'];
+        const selectClauses = notes.map((note) => `SUM(CASE WHEN grade = '${note}' THEN grade_number ELSE 0 END) AS '${note}'`);
+        const selectClause = selectClauses.join(', ');
+
+        const [overall, courses] = await Promise.all([
+            queryDatabase(`SELECT ${selectClause} FROM courses WHERE professor = ? GROUP BY professor;`, [`${professor}`]),
+            queryDatabase(`SELECT title, acronym, ${selectClause}, GROUP_CONCAT(DISTINCT session ORDER BY session ASC SEPARATOR ', ') AS sessions FROM courses WHERE professor = ? GROUP BY title,acronym;`, [`${professor}`])
+        ]);
+
+        res.status(200).json({ overall: overall[0], courses });
+
+    } catch (err) {
+        res.status(500).json({ message: "Internal Server Error." + err });
     }
 
 };
