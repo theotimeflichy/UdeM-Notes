883cf4de27437053a481c1b6c0452492b391d098
Théotime <tronaidev@gmail.com>
2023-10-07 14:15:00 -0500

    Update professor.controller.js

--- api/controllers/professor.controller.js
+++ api/controllers/professor.controller.js
@@ -11,10 +11,14 @@ module.exports.getProfessor = async (req, res) => {
         const selectClauses = notes.map((note) => `SUM(CASE WHEN grade = '${note}' THEN grade_number ELSE 0 END) AS '${note}'`);
         const selectClause = selectClauses.join(', ');
 
+        // Permet de calculer la note la plus fréquente.
+        const mostFrequentGrade = `(SELECT grade FROM courses WHERE professor = ? ORDER BY grade_number DESC LIMIT 1) AS mostFrequentGrade`;
+        const mostFrequentGradeCourse = `(SELECT grade FROM courses WHERE title = title AND professor = ? ORDER BY grade_number DESC LIMIT 1) AS mostFrequentGrade`;
+
         // On calcul les résultats totaux et par professeur.
         const [overall, courses] = await Promise.all([
-            queryDatabase(`SELECT ${selectClause} FROM courses WHERE professor = ? GROUP BY professor;`, [`${professor}`]),
-            queryDatabase(`SELECT title, acronym, ${selectClause}, GROUP_CONCAT(DISTINCT session ORDER BY session ASC SEPARATOR ', ') AS sessions FROM courses WHERE professor = ? GROUP BY title,acronym;`, [`${professor}`])
+            queryDatabase(`SELECT ${selectClause}, ${mostFrequentGrade} FROM courses WHERE professor = ? GROUP BY professor;`, [`${professor}`, `${professor}`]),
+            queryDatabase(`SELECT title, acronym, ${selectClause}, ${mostFrequentGradeCourse}, GROUP_CONCAT(DISTINCT session ORDER BY session ASC SEPARATOR ', ') AS sessions FROM courses WHERE professor = ? GROUP BY title,acronym;`, [`${professor}`, `${professor}`])
         ]);
 
         res.status(200).json({ overall: overall[0], courses });
