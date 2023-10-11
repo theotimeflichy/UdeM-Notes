2fd961d97a56334629b4c61ee35ab7ba442d8bc5
Théotime Flichy <31313380+theotimeflichy@users.noreply.github.com>
2023-10-11 10:29:03 -0500

    Update server.js

--- api/server.js
+++ api/server.js
@@ -6,7 +6,7 @@ const port = 5000;
 
 // Middlewares
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173'); // Remplacez par l'URL de votre application React.js
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173'); 
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     next();
